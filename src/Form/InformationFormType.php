5898e6318ddef4630eaa2d9db2b5e6514d979395
Florent <floderouet@laposte.net>
2023-01-19 11:33:30 +0100

    add img and styles css file

--- src/Form/InformationFormType.php
+++ src/Form/InformationFormType.php
@@ -3,6 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Information;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,13 +15,19 @@ class InformationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('firstname')
-            ->add('email')
-            ->add('phone_number')
-            ->add('city')
-            ->add('film')
-            ->add('citation')
+            ->add('name', null, ['label' => 'Nom*'])
+            ->add('firstname', null, ['label' => 'Prénom*'])
+            ->add('email', EmailType::class, ['label' => 'Email*'])
+            ->add('phone_number', null, ['label' => 'Téléphone (+33)',  'required' => false])
+            ->add('city', null, ['label' => 'Ville*'])
+            ->add('film', null, ['label' => 'Film*'])
+            ->add('citation', TextareaType::class,[
+                'required' => false,
+                'attr' => [
+                    'placeholder' => "Est-ce qu'un fenouil c'est redondant ?",
+                ],
+            ] )
+            ->add('Soumettre', SubmitType::class)
         ;
     }
 
